868f41da9c8b82cb9734ab179a6b638162733b73
leah-mcnabb <leahmaemcnabb@gmail.com>
2026-07-15 15:54:17 -0400

    hello

---
+++ Spreadsheet.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Spreadsheet {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello");
+        String sheetId = "1mdOiBJqmZvHPYF6hv3Ba1wBR8pLT4l_0grsS_nQh7jc";
+        String urlString = "https://docs.google.com/spreadsheets/d/" + sheetID + "/export?format=csv";
+
+        URL url = new URL(urlString);
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()))){
+            String line;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
+}
