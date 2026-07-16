30103bec613681071a3b10f1a067b16770aa412b
blue-or-pink <jane.carol.berry@gmail.com>
2026-07-16 14:37:36 -0400

    wip

---
+++ SuperDuperWordGenerator.java
@@ -0,0 +1,36 @@
+public class SuperDuperWordGenerator {
+
+    private static final String[] charList = {
+        "p",
+        "b",
+        "t",
+        "d",
+        "k",
+        "g",
+        "m",
+        "n",
+        "ŋ",
+        "f",
+        "v",
+        "s",
+        "z",
+        "ʃ",
+        "ʒ",
+        "h",
+        "ɹ",
+        "j",
+        "l",
+        "i",
+        "u",
+        "ɪ",
+        "ʊ",
+        "ə",
+        "ɛ",
+        "ɔ",
+        "æ",
+        "ɑ"
+    };
+    public static void main(String[] args) throws Exception {
+
+    }
+}
