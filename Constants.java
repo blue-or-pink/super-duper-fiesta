05a8b2a6532f1d81b681d361f1bc3c8ef704334a
blue-or-pink <jane.carol.berry@gmail.com>
2026-07-18 15:52:30 -0400

    wip

--- Constants.java
+++ Constants.java
@@ -1,7 +1,7 @@
 public class Constants {
-    public static final int pronounsListLength = 48;
+    public static final int pronounsListLength = 49;
     public static final int adjectivesListLength = 50;
     public static final int prepositionsListLength = 70;
     public static final int verbsListLength = 50;
-    public static final int nounsListLength = 74;
+    public static final int nounsListLength = 75;
 }
